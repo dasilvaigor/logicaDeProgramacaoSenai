6d2be343e4bf122badba660aebc7e2e485709eb7
Igor Silva <igorss.is@hotmail.com>
2023-05-24 21:18:32 -0300

    Add files via upload

---
+++ C/aula02/atividade/atividade01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	
+	int numero;
+	
+	printf("Informe um numero: ");
+	scanf("%d", &numero);
+	
+	system("cls");
+	
+	if (numero > 10) {
+		printf("O numero digitado e maior que 10!\n");
+	} else {
+		printf("O numero digitado nao e maior que 10!\n");
+	}
+	
+	system("pause");
+	return 0;
+}
